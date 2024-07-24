6f047af8403d6ba0c3b720cdecb58b329dbd4ef6
dale <dale@hmdtrucking.com>
2024-07-24 11:18:05 -0500

    Simple API Endpoint

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, session, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
 
-@app.route('/')
-def index():
-    return f"App Config: {app.config['SECRET_KEY']}"
-
-@app.route('/set_global')
-def set_global():
-    session['message'] = "Hello, this is a global variable!"
-    return "Global variable set!"
-
-@app.route('/get_global')
-def get_global():
-    message = session.get('message', "No global variable set!")
-    return message
+@app.route('/api/hello', methods=['GET'])
+def hello_api():
+    return jsonify(message="Hello, World!")
 
 if __name__ == "__main__":
-    with app.app_context():
-        print(f"App Context: {app.config['SECRET_KEY']}")
     app.run(debug=True)
